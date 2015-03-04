1fd099fe44cf60591776aab60b77d005e2cb98ca
Martin Kim Dung-Pham <kim@elbedev.com>
2015-03-04 20:42:51 +0100

    Updated app description and commands

--- xing/xing.go
+++ xing/xing.go
@@ -7,26 +7,26 @@ import (
 
 func main() {
 	app := NewApp(*cli.NewApp())
-	app.Name = "xing"
-	app.Usage = "xing on the command line"
+	app.Name = "XING cli"
+	app.Usage = "xing"
 	app.Author = "Martin Kim Dung-Pham"
 	app.Email = "kim.dung-pham@xing.com"
 	app.Version = "0.0.1"
 	app.Commands = []cli.Command{
 		{
-			Name:      "me",
+			Name:      "profile",
 			ShortName: "p",
 			Usage:     "Get my profile",
 			Action: app.loadMeAction,
 		},
 		{
-			Name:      "Contacts",
+			Name:      "contacts",
 			ShortName: "c",
 			Usage:     "Get contacts for the given user id: c <userId>",
 			Action: app.LoadContactsAction,
 		},
 		{
-			Name:      "Messages",
+			Name:      "messages",
 			ShortName: "m",
 			Usage:     "Get messages for the given user id: c <userId>",
 			Action: app.LoadMessagesAction,
